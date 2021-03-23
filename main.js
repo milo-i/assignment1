de5704371fb23baa06ca9e53bed68b8464c45c01
Igor Milosevic <igor.milosevic1988@gmail.com>
2021-03-23 07:44:44 +0100

    solves the assignment

---
+++ main.js
@@ -0,0 +1,35 @@
+// KOD 1
+
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+
+for (let i = 0; i < cars.length; i++) {
+ console.log(cars[i]);
+}
+
+
+// KOD 2
+
+let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let fruits = ['apple', 'banana', 'orange'];
+
+for (let i = 0; i < cars.length; i++) {
+ console.log(cars[i]);
+}
+
+
+for (let i = 0; i < fruits.length; i++) {
+ console.log(fruits[i]);
+}
+
+
+// KOD 3
+
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];
+
+function logFlower(flowers) {
+ for (let i = 0; i < flowers.length; i++) {
+  console.log(flowers[i]);
+ }
+}
+
+logFlower(flowers);
